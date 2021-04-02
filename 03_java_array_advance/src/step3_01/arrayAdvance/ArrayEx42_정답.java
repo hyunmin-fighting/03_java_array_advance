7f6c39101a82d8185fa387ef15531147fc1ba266
hyunmin <hyunmin@KRD111188.LGDISPLAY.GLOBAL>
2021-04-02 17:44:52 +0900

    12313213

---
+++ 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx42_정답.java
@@ -0,0 +1,38 @@
+package step3_01.arrayAdvance;
+
+import java.util.Scanner;
+
+// #오목
+
+public class ArrayEx42_정답 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int size = 10;               
+		int[][] omok = {
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0}
+		};
+
+		int p1Y = 0;
+		int p1X = 0;
+		int p2Y = 0;
+		int p2X = 0;
+		int turn = 0;
+		int win = 0;
+		
+		
+		
+		
+	}
+}
---
+++ 03_java_array_advance/src/step3_01/arrayAdvance/Test.java
@@ -0,0 +1,17 @@
+package step3_01.arrayAdvance;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	
+	int[][] arr = {{0,0,0},{0,0,0},{0,0,0}};
+	
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+}
